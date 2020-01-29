e02fd0ee37cea398c25be8181af0f13afeac2f23
emilylogan <49258718+emilylogan@users.noreply.github.com>
2020-01-29 14:36:37 -0800

    style

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 console.log('OK');
 
 // Initialize Plyr player
-const player = new Plyr('#player');
+
+const player = new Plyr( '#player', {
+  
+    controls : ['play', 'current-time', 'progress'],
+    
+    keyboard : { focused: true, global: true },
+    
+    seekTime : 30
+  });
+
+  console.log( player );
